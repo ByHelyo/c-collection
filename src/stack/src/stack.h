93abeff192ca6c75b31f36f567501649fffef592
Franck Hu <franck.hu@epita.fr>
2022-04-19 13:52:54 +0200

    Stack
    add documentation

--- src/stack/src/stack.h
+++ src/stack/src/stack.h
@@ -12,14 +12,46 @@ struct Stack
     size_t size;
 };
 
+/**
+ * Build a stack
+ *
+ * @return stack built
+ */
 struct Stack stack_build();
 
+/**
+ * Return the size of the stack
+ *
+ * @param stack
+ * @return size
+ */
 size_t stack_size(struct Stack *stack);
 
+/**
+ * Push ''element'' at the head of the stack
+ *
+ * @param stack
+ * @param elt
+ */
 void stack_push(struct Stack *stack, void *elt);
 
+/**
+ * Pop an element at the head of the stack and return it
+ *
+ * @param stack
+ * @return popped element
+ *         NULL if the stack is empty
+ */
 void *stack_pop(struct Stack *stack);
 
+/**
+ * Clear the stack
+ *
+ * A free function can be provided to free each element otherwise ''free_function''  must be set to NULL
+ *
+ * @param stack
+ * @param free_function
+ */
 void stack_clear(struct Stack *stack, void (*free_function)(void *));
 
 #endif // C_COLLECTION_STACK_H
