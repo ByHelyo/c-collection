6b25d2ab0a248b7c3c42ba62f9d8ba8562fb1ae8
Franck Hu <franck.hu@epita.fr>
2022-05-20 02:50:32 +0200

    LinkedList
    tests linkedList_pop()

---
+++ src/linkedlist/test/test-linkedlist-pop.c
@@ -0,0 +1,122 @@
+#include "../../node.h"
+#include "../src/linkedlist.h"
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(linkedList_remove, err_pop_NULL, .exit_code = 1, .init = cr_redirect_stderr)
+{
+    struct LinkedList *linked_list = NULL;
+
+    linkedList_pop(linked_list);
+}
+
+Test(linkedList_remove, popped_size_1)
+{
+    struct LinkedList linked_list = linkedList_build();
+
+    int push_elt1 = 1;
+
+    linkedList_push(&linked_list, &push_elt1);
+
+    int *popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt1, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt1, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 0);
+}
+
+Test(linkedList_remove, popped_size_2)
+{
+    struct LinkedList linked_list = linkedList_build();
+
+    int push_elt1 = 1;
+    int push_elt2 = 2;
+
+    linkedList_push(&linked_list, &push_elt1);
+    linkedList_push(&linked_list, &push_elt2);
+
+    int *popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt2, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt2, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 1);
+
+    popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt1, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt1, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 0);
+}
+
+Test(linkedList_remove, popped_size_3)
+{
+    struct LinkedList linked_list = linkedList_build();
+
+    int push_elt1 = 1;
+    int push_elt2 = 2;
+    int push_elt3 = 3;
+
+    linkedList_push(&linked_list, &push_elt1);
+    linkedList_push(&linked_list, &push_elt2);
+    linkedList_push(&linked_list, &push_elt3);
+
+    int *popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt3, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt3, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 2);
+
+    popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt2, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt2, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 1);
+
+    popped_elt = linkedList_pop(&linked_list);
+
+    // Check popped element
+
+    // Check address
+    cr_assert_eq(&push_elt1, popped_elt);
+
+    // Check value
+    cr_assert_eq(push_elt1, *popped_elt);
+
+    // Check linked_list->size
+    cr_assert_eq(linked_list.size, 0);
+}
